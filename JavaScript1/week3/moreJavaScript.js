b260b1ba64ba6f2d0329cde730f97173356555f4
Parichat <ppleparichat@gmail.com>
2023-03-21 18:58:31 +0100

    Update Homework

--- JavaScript1/week3/moreJavaScript.js
+++ JavaScript1/week3/moreJavaScript.js
@@ -1,18 +1,18 @@
-// 1. Create a function that takes 3 arguments and returns the sum of the these arguments.
+// 1. 
 function sumNumber(num1, num2, num3) {
     let sum =  num1+num2+num3
     return sum
 }
 sumNumber(2,3,4) // 9
 
-// 2. Create a function named colorCar that receives a color, and prints out, 'a red car' for example.
+// 2. 
 function colorCar(color) {
     let colorCar = `a ${color} car`
     return colorCar
 }
 colorCar('red')
 
-// 3. Create an object and a function that takes the object as a parameter and prints out all of its properties and values.
+// 3. 
 const car = {
     type:"Toyota", 
     model:"500", 
@@ -24,7 +24,7 @@ function carPrint(param) {
 }
 carPrint(car)
 
-// 4. Create a function named vehicleType that receives a color, and a code, 1 for car, 2 for motorbike. And prints 'a blue motorbike' for example when called as vehicleType("blue", 2)
+// 4. 
 function vehicleType(color, code) {
     if (code === 1){
         return `a ${color} motorbike`
@@ -34,15 +34,10 @@ function vehicleType(color, code) {
 }
 vehicleType("blue",2) // 'a blue motorbike'
 
-// 5. Can you write the following without the if statement, but with just as a single line with console.log(...);?
-// if (3 === 3) {
-//     console.log("yes");
-//   } else {
-//     console.log("no");
-//   }
+// 5. 
 3 === 3 ? console.log("yes") : console.log("no") 
 
-// 6. Create a function called vehicle, like before, but takes another parameter called age, so that vehicle("blue", 1, 5) prints 'a blue used car'
+// 6. 
 function vehicle(color, code, age) {
     if (code === 1 && age > 1){
         return `a ${color} used car`
@@ -50,33 +45,59 @@ function vehicle(color, code, age) {
 }
 vehicle("blue", 1, 5) // 'a blue used car'
 
-// 7. Make a list of vehicles, you can add "motorbike", "caravan", "bike", or more.
+// 7. 
 let vehicles = ["motorbike", "caravan", "bike", "car"];
 
-// 8. How do you get the third element from that list?
+// 8. 
 console.log(vehicles[2]) // bike
 
-// 9. Change the function vehicle to use the list of question 7. So that vehicle("green", 3, 1) prints "a green new bike".
+// 9. 
+function vehicleTwo(color, code, age) {
+  let car = vehicles[code -1];
+  let vehicleAge = age > 0 ? "used" : "new";
+  console.log(`a ${color} ${vehicleAge} ${car}`);
+};
+vehicleTwo("green", 3, 1);
 
+// 10. 
+let advertisement = `Amazing Joe's Garage, we service`
 
-// 10. Use the list of vehicles to write an advertisement. So that it prints something like: "Amazing Joe's Garage, we service cars, motorbikes, caravans and bikes.". (Hint: use a for loop.)
+// 11. 
 
-// 11. What if you add one more vehicle to the list, can you have that added to the advertisement without changing the code for question 10?
-
-// 12. Create an empty object.
+// 12. 
 let person = {}
 
-// 13. Create an object that contains the teachers that you have had so far for the different modules.
+// 13. 
 let myTeachers = {
     anna : 'reading',
     john : 'writing',
     mimi : 'history'
 }
 
-// 14. Add a property to the object you just created that contains the languages that they have taught you.
+// 14. 
+myTeachers['Tommy'] = 'HTML'
+myTeachers['Sahin'] = 'JavaScript'
 
-// 15. Add a property to the object you just created that contains the languages that they have taught you.
+console.log(myTeachers)
 
-// 16. Take a look at the following code:
+// 15. 
+let x = [1, 2, 3];
+let y = [1, 2, 3];
+let z = y;
+// I think x == y, x === y and z == y and z == x, They are equal
 
-// 17. What does the following code return? (And why?)
+if (x == y && x === y && z == y && z == x) {
+    console.log("TRUE")
+} else {
+    console.log("FALSE") // FALSE
+}
+// They are not equal
+
+// 16. 
+let o1 = { foo: "bar" };
+let o2 = { foo: "bar" };
+let o3 = o2;
+
+// 17. 
+let bar = 42;
+typeof typeof bar;
