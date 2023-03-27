3f5f2e42f12bc518c0e4256ea58a02c5bf06ea27
Parichat <ppleparichat@gmail.com>
2023-03-27 15:05:33 +0200

    homework update

--- JavaScript2/week1/apps.js
+++ JavaScript2/week1/apps.js
@@ -1,5 +1,29 @@
-const myBooks = ['anna-Karenina', 'madame-Bovary', 'war-and-peace', 
-'the-great-gatsby', 'lolita' , 'middlemarch', 'the-adventures-of-huckleberry-finn',
- 'The-stories-of-anton-chekhov', 'in-search-of-lost-time', 'hamlet']
+const myBooks = [
+  "anna-Karenina",
+  "madame-Bovary",
+  "war-and-peace",
+  "the-great-gatsby",
+  "lolita",
+  "middle-march",
+  "the-adventures-of-huckleberry-finn",
+  "The-stories-of-anton-chekhov",
+  "in-search-of-lost-time",
+  "hamlet",
+];
 
- console.log(myBooks)
+(function(){
+  var ul = document.createElement('ul');
+  ul.setAttribute('id','proList');
+  
+  document.querySelector('body').appendChild(ul);
+  myBooks.forEach(renderProductList);
+
+  function renderProductList(element, index, arr) {
+      var li = document.createElement('li');
+      li.setAttribute('class','item');
+
+      ul.appendChild(li);
+
+      li.innerHTML=li.innerHTML + element;
+  }
+})();
