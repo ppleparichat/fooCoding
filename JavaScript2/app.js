cf54046c17b54a5e32bde98c48cc3f177d2cdf37
Parichat <ppleparichat@gmail.com>
2023-03-27 15:21:35 +0200

    Create .js

---
+++ JavaScript2/app.js
@@ -0,0 +1,13 @@
+const myBooks = [
+    "anna-Karenina",
+    "madame-Bovary",
+    "war-and-peace",
+    "the-great-gatsby",
+    "lolita",
+    "middle-march",
+    "the-adventures-of-huckleberry-finn",
+    "The-stories-of-anton-chekhov",
+    "in-search-of-lost-time",
+    "hamlet",
+  ];
+  
