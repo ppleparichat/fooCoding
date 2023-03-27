2312acd01c14a274910dc5f59eccae36633e1bdb
Parichat <ppleparichat@gmail.com>
2023-03-27 11:50:07 +0200

    homework

---
+++ JavaScript2/week1/apps.js
@@ -0,0 +1,5 @@
+const myBooks = ['anna-Karenina', 'madame-Bovary', 'war-and-peace', 
+'the-great-gatsby', 'lolita' , 'middlemarch', 'the-adventures-of-huckleberry-finn',
+ 'The-stories-of-anton-chekhov', 'in-search-of-lost-time', 'hamlet']
+
+ console.log(myBooks)
