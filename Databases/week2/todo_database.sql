dd370f4337dbfc38673677ba8f0a1964a6c4d5d9
Parichat <ppleparichat@gmail.com>
2023-05-31 13:18:12 +0200

    update ERD.png and sql code

--- Databases/week2/todo_database.sql
+++ Databases/week2/todo_database.sql
@@ -14,19 +14,18 @@ CREATE TABLE todo_list (
     FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
 
-CREATE TABLE todo_item (
+CREATE TABLE tag (
+    tag_id INT PRIMARY KEY AUTO_INCREMENT,
+    tag_name VARCHAR(255)
+);
+
+CREATE TABLE item_list (
     item_id INT PRIMARY KEY AUTO_INCREMENT,
     item_name VARCHAR(255),
     reminder_date DATETIME,
     completed BOOLEAN,
     list_id INT,
-    FOREIGN KEY (list_id) REFERENCES todo_list(list_id)
-);
-
-CREATE TABLE tag (
-    item_id INT PRIMARY KEY AUTO_INCREMENT,
-    tag_name VARCHAR(255),
     tag_id INT,
-    FOREIGN KEY (item_id) REFERENCES todo_item(item_id)
+    FOREIGN KEY (list_id) REFERENCES todo_list(list_id),
+    FOREIGN KEY (tag_id) REFERENCES tag(tag_id)
 );
-
